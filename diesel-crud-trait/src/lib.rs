ae71d6988ce2690d6f862da1397b0e07755eb540
Louis-Marie Baer <lm@baermail.fr>
2024-02-23 14:06:47 +0100

    experiment simplify trait bounds
    trying to take traits from the macro table! and derives for the struct representing the table.

--- diesel-crud-trait/src/lib.rs
+++ diesel-crud-trait/src/lib.rs
@@ -1,119 +1,129 @@
 #![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
-use diesel::{
-    associations::HasTable, deserialize::FromSql, dsl::Desc, expression::{NonAggregate, ValidGrouping, MixedAggregates, is_aggregate::No}, query_builder::{AsQuery, QueryFragment, QueryId}, query_dsl::{methods::{FilterDsl, FindDsl, LimitDsl, LoadQuery, OrderDsl, SelectDsl}, UpdateAndFetchResults}, sql_types::SingleValue, sqlite::Sqlite, AsChangeset,   Identifiable, Insertable, QueryDsl, QuerySource, RunQueryDsl, SaveChangesDsl, SelectableExpression, SqliteConnection, Table
-};
-use diesel::Queryable;
-use diesel::query_builder::{IntoUpdateTarget, UpdateStatement};
-use diesel::EqAll;
-use error::ErrorCrud;
-use diesel::sql_types::HasSqlType;
+
+#[cfg(feature = "derive")]
+pub extern crate diesel_crud_trait_derive;
+use diesel::backend::Backend;
 use diesel::insertable::CanInsertInSingleQuery;
-mod error;
-use diesel::query_dsl::methods::ExecuteDsl;
+use diesel::internal::table_macro::AliasAliasAppearsInFromClause;
+use diesel::internal::table_macro::AliasAppearsInFromClause;
+use diesel::internal::table_macro::FieldAliasMapperAssociatedTypesDisjointnessTrick;
+use diesel::internal::table_macro::NoFromClause;
+use diesel::internal::table_macro::StaticQueryFragment;
+use diesel::query_builder::{IntoUpdateTarget, UpdateStatement};
+use diesel::query_source::Alias;
+use diesel::query_source::AliasSource;
+use diesel::query_source::AppearsInFromClause;
+use diesel::sql_types::HasSqlType;
+use diesel::Column;
+use diesel::EqAll;
+use diesel::JoinTo;
+use diesel::Queryable;
+use diesel::Selectable;
+use diesel::{
+    associations::HasTable,
+    deserialize::FromSql,
+    dsl::Desc,
+    expression::{is_aggregate::No, MixedAggregates, NonAggregate, ValidGrouping},
+    query_builder::{AsQuery, QueryFragment, QueryId},
+    query_dsl::{
+        methods::{FilterDsl, FindDsl, LimitDsl, LoadQuery, OrderDsl, SelectDsl},
+        UpdateAndFetchResults,
+    },
+    sql_types::SingleValue,
+    sqlite::Sqlite,
+    AsChangeset, Identifiable, Insertable, QueryDsl, QuerySource, RunQueryDsl, SaveChangesDsl,
+    SelectableExpression, SqliteConnection, Table,
+};
+use error::ErrorCrud;
+use std::fmt::Debug;
+pub mod error;
+use diesel::internal::derives::insertable::UndecoratedInsertRecord;
 use diesel::query_builder::DeleteStatement;
+use diesel::query_dsl::methods::ExecuteDsl;
+
 /// the trait CrudAble doc todo
-pub trait CrudAble  
+pub trait CrudAble
 where
-    Self: Insertable<Self::Table> + 'static + Sized + HasTable,
-    &'static Self: Insertable<Self::Table> + Identifiable + SaveChangesDsl<SqliteConnection> + AsChangeset + IntoUpdateTarget + HasTable,
-    <Self::Table as QuerySource>::FromClause: QueryFragment<Sqlite>,
-    <&'static Self as Insertable<Self::Table>>::Values: QueryFragment<Sqlite> + QueryId + CanInsertInSingleQuery<Sqlite>,
-    Self::Table: QueryId + FindDsl<i32> + Identifiable,
-    <Self::Table as AsQuery>::Query: SelectDsl<Self::IdColumn> +  FilterDsl<i32>,
-    <<Self::Table as AsQuery>::Query as SelectDsl<Self::IdColumn>>::Output: QueryDsl + OrderDsl<Desc<Self::IdColumn>>,
-    <<<Self::Table as AsQuery>::Query as SelectDsl<Self::IdColumn>>::Output as OrderDsl<Desc<Self::IdColumn>>>::Output: AsQuery + LimitDsl + RunQueryDsl<SqliteConnection>,
-<<<<Self::Table as AsQuery>::Query as SelectDsl<Self::IdColumn>>::Output as OrderDsl<Desc<Self::IdColumn>>>::Output as LimitDsl>::Output: RunQueryDsl<SqliteConnection> + LoadQuery<'static,SqliteConnection, i32>,
-<Self::Table as FindDsl<i32>>::Output: RunQueryDsl<SqliteConnection> + LimitDsl + HasTable + IntoUpdateTarget,
-<<Self::Table as FindDsl<i32>>::Output as LimitDsl>::Output: LoadQuery<'static, SqliteConnection, Self>,
-<<&'static Self as HasTable>::Table as AsQuery>::Query: FilterDsl<i32>,
-<<<&'static Self as HasTable>::Table as AsQuery>::Query as FilterDsl<i32>>::Output: LoadQuery<'static, SqliteConnection, Self>,
-<<&'static Self as HasTable>::Table as Table>::PrimaryKey: EqAll<<&'static Self as Identifiable>::Id>,
-<<<Self as HasTable>::Table as Table>::PrimaryKey as ValidGrouping<()>>::IsAggregate: MixedAggregates<diesel::expression::is_aggregate::No>,
-<<<&'static Self as HasTable>::Table as Table>::AllColumns as ValidGrouping<()>>::IsAggregate: MixedAggregates<diesel::expression::is_aggregate::No>,
-<<<<&'static Self as HasTable>::Table as Table>::AllColumns as ValidGrouping<()>>::IsAggregate as MixedAggregates<diesel::expression::is_aggregate::No>>::Output: MixedAggregates<No>,
-<<&'static Self as HasTable>::Table as AsQuery>::Query: FilterDsl<<<<&'static Self as HasTable>::Table as Table>::PrimaryKey as EqAll<<&'static Self as Identifiable>::Id>>::Output>,
-<<<&'static Self as HasTable>::Table as AsQuery>::Query as FilterDsl<<<<&'static Self as HasTable>::Table as Table>::PrimaryKey as EqAll<<&'static Self as Identifiable>::Id>>::Output>>::Output: LoadQuery<'static, SqliteConnection, Self>,
-    <<Self::Table as Table>::PrimaryKey as diesel::Expression>::SqlType: SingleValue + QueryId,
-    <Self::Table as Table>::PrimaryKey: QueryFragment<Sqlite> + QueryId + NonAggregate,
-UpdateStatement<<<<Self as HasTable>::Table as FindDsl<i32>>::Output as HasTable>::Table, <<<Self as HasTable>::Table as FindDsl<i32>>::Output as IntoUpdateTarget>::WhereClause, <<<Self as HasTable>::Table as FindDsl<i32>>::Output as HasTable>::Table>: AsQuery,  UpdateStatement<<<<Self as HasTable>::Table as FindDsl<i32>>::Output as HasTable>::Table, <<<Self as HasTable>::Table as FindDsl<i32>>::Output as IntoUpdateTarget>::WhereClause, <&'static Self as AsChangeset>::Changeset>: AsQuery
+    Self: 'static
+        + Sized
+        + HasTable
+        + Queryable<<Self::Table as AsQuery>::SqlType, Sqlite>
+        + Selectable<Sqlite>
+        + Insertable<Self::Table>
+        + UndecoratedInsertRecord<Self::Table>
+        + AsChangeset
+        + Clone
+        + Debug,
+    &'static Self: Identifiable + Insertable<Self::Table> + AsChangeset,
+    Self::Table: Debug
+        + Clone
+        + Copy
+        + QueryId
+        + Default
+        + QuerySource
+        + QueryFragment<Sqlite>
+        + StaticQueryFragment
+        + AsQuery
+        + Table
+        + HasTable
+        + IntoUpdateTarget
+        + AppearsInFromClause<Self::Table>, // + AliasAppearsInFromClause<Alias<Self::Table>, Self::Table>
+    // + AliasAliasAppearsInFromClause<Self::Table, Alias<Self::Table>, Alias<Self::Table>>
+    // + AppearsInFromClause<Alias<Self::Table>>, // + FieldAliasMapperAssociatedTypesDisjointnessTrick<Self::Table, Self::Table, Self::Table>,
+    NoFromClause: AppearsInFromClause<Self::Table>,
 {
-    /// type of the column with primary key. like schemas::table_name::rowid
-    /// will be determined automaticly by the derive trait
-    // type IdColumn: ExpressionMethods + Expression + SelectableExpression<Self::Table> + NonAggregate;
-    type IdColumn: SelectableExpression<Self> + ValidGrouping<()> + QueryId + QueryFragment<Sqlite> + NonAggregate;
-    /// type of the table. like schemas::table_name::table
-    const TABLE: Self::Table;
-    //  = Self::table();
-    /// type of the column with primary key. like schemas::table_name::rowid
-    /// will be determined automaticly by the derive trait
-    const ID_COLUMN: <Self::Table as Table>::PrimaryKey;
-    // = Self::table().primary_key();
-    
-    /// method create of CRUD. correspond to POST /elements for REST API.
-    fn create_with_check(&'static self, conn: &mut SqliteConnection, check: Option<Box<dyn FnMut(&Self, &mut SqliteConnection) -> Result<(), ErrorCrud>>>) -> Result<i32, ErrorCrud> where 
-        <Self::IdColumn as diesel::Expression>::SqlType: SingleValue, 
-     i32: FromSql<<<Self::Table as Table>::PrimaryKey as diesel::Expression>::SqlType, Sqlite>,
-    Sqlite: HasSqlType<<<Self::Table as Table>::PrimaryKey as diesel::Expression>::SqlType>,
-    {
-        // check data if closure was given for it, return error of check if it failed or continue.
-        if let Some(mut checking) = check {
-            checking(&self, conn)?;
-        }
-        // insert the new data and return the id
-        Ok(diesel::insert_into(Self::TABLE).values(self).returning(Self::ID_COLUMN).get_result(conn)?)
-    }
-    /// wrapper for create_with_check without providing a check.
-    /// It exist to make a simpler usage.
-    fn create(&'static self, conn: &mut SqliteConnection) -> Result<i32, ErrorCrud> where 
-    <Self::IdColumn as diesel::Expression>::SqlType: SingleValue, 
-    i32: FromSql<<<Self::Table as Table>::PrimaryKey as diesel::Expression>::SqlType, Sqlite>,
-    Sqlite: HasSqlType<<<Self::Table as Table>::PrimaryKey as diesel::Expression>::SqlType>,
-    
-    {
-        self.create_with_check(conn, None)
-    }
-    /// method read of CRUD. correspond to GET /elements/id for REST API.
-    fn read(rowid: i32, conn: &mut SqliteConnection) -> Result<Self, ErrorCrud> {
-        Ok(QueryDsl::find(Self::TABLE, rowid).first::<Self>(conn)?)
-    }
-    /// method update of CRUD. correspond to PUT or PATCH /elements/id for REST API.
-    /// The model needs to have a rowid
-    fn update_with_check(&'static self, conn: &mut SqliteConnection, check: Option<Box<dyn FnMut(&Self, &mut SqliteConnection) -> Result<(), ErrorCrud>>>) -> Result<Self, ErrorCrud> 
-    where 
-    UpdateStatement<<&'static Self as HasTable>::Table, <&'static Self as IntoUpdateTarget>::WhereClause, <&'static Self as AsChangeset>::Changeset>: QueryFragment<Sqlite>,
-    SqliteConnection: UpdateAndFetchResults<&'static Self, Self>
-    
-{
-        // check data if closure was given for it before updating.
-        if let Some(mut checking) = check {
-            checking(self, conn)?;
-        }
-        Ok(SaveChangesDsl::save_changes::<Self>(self, conn)?)
- }
-    /// wrapper for update_with_check without providing a check.
-    /// It exist to make a simpler usage.
-    fn update(&'static self, conn: &mut SqliteConnection) -> Result<Self, ErrorCrud> where 
-    UpdateStatement<<&'static Self as HasTable>::Table, <&'static Self as IntoUpdateTarget>::WhereClause, <&'static Self as AsChangeset>::Changeset>: QueryFragment<Sqlite>,
-    SqliteConnection: UpdateAndFetchResults<&'static Self, Self>
-    {
-        self.update_with_check(conn, None)
-    }
-    #[cfg(feature="methods_on_all")]
-    /// return all rows of table. correspont to GET /elements
-    fn list_all(conn: &mut SqliteConnection) -> Result<Vec<Self>, ErrorCrud> where 
-    <Self::Table as AsQuery>::Query: QueryFragment<Sqlite> + QueryId,
-    <Self::Table as AsQuery>::SqlType: SingleValue,
-    Self: Queryable<<Self::Table as AsQuery>::SqlType, Sqlite>,
-    Sqlite: HasSqlType<<Self::Table as AsQuery>::SqlType>
-    {
-        Ok(Self::TABLE.load(conn)?)
-    }
-    #[cfg(feature="methods_on_all")]
-    /// empty the table. correspont to DELETE /elements
-    fn delete_all(conn: &mut SqliteConnection) -> Result<usize, ErrorCrud> where 
-    Self::Table: IntoUpdateTarget,
-    DeleteStatement<<<Self as HasTable>::Table as HasTable>::Table, <<Self as HasTable>::Table as IntoUpdateTarget>::WhereClause>: ExecuteDsl<SqliteConnection, Sqlite>     {
-        Ok(diesel::delete(Self::TABLE).execute(conn)?)
-    }
+    // /// method create of CRUD. correspond to POST /elements for REST API.
+    // fn create_with_check(
+    //     &'static self,
+    //     conn: &mut SqliteConnection,
+    //     check: Option<Box<dyn FnMut(&Self, &mut SqliteConnection) -> Result<(), ErrorCrud>>>,
+    // ) -> Result<i32, ErrorCrud> {
+    //     // check data if closure was given for it, return error of check if it failed or continue.
+    //     if let Some(mut checking) = check {
+    //         checking(&self, conn)?;
+    //     }
+    //     // insert the new data and return the id
+    //     Ok(diesel::insert_into(Self::table())
+    //         .values(self)
+    //         .returning(Self::table().primary_key())
+    //         .get_result(conn)?)
+    // }
+    // /// wrapper for create_with_check without providing a check.
+    // /// It exist to make a simpler usage.
+    // fn create(&'static self, conn: &mut SqliteConnection) -> Result<i32, ErrorCrud> {
+    //     self.create_with_check(conn, None)
+    // }
+    // /// method read of CRUD. correspond to GET /elements/id for REST API.
+    // fn read(rowid: i32, conn: &mut SqliteConnection) -> Result<Self, ErrorCrud> {
+    //     Ok(QueryDsl::find(Self::table(), rowid).first::<Self>(conn)?)
+    // }
+    // /// method update of CRUD. correspond to PUT or PATCH /elements/id for REST API.
+    // /// The model needs to have a rowid
+    // fn update_with_check(
+    //     &'static self,
+    //     conn: &mut SqliteConnection,
+    //     check: Option<Box<dyn FnMut(&Self, &mut SqliteConnection) -> Result<(), ErrorCrud>>>,
+    // ) -> Result<Self, ErrorCrud> {
+    //     // check data if closure was given for it before updating.
+    //     if let Some(mut checking) = check {
+    //         checking(self, conn)?;
+    //     }
+    //     Ok(SaveChangesDsl::save_changes::<Self>(self, conn)?)
+    // }
+    // /// wrapper for update_with_check without providing a check.
+    // /// It exist to make a simpler usage.
+    // fn update(&'static self, conn: &mut SqliteConnection) -> Result<Self, ErrorCrud> {
+    //     self.update_with_check(conn, None)
+    // }
+    // #[cfg(feature = "methods_on_all")]
+    // /// return all rows of table. correspont to GET /elements
+    // fn list_all(conn: &mut SqliteConnection) -> Result<Vec<Self>, ErrorCrud> {
+    //     Ok(Self::table().load(conn)?)
+    // }
+    // #[cfg(feature = "methods_on_all")]
+    // /// empty the table. correspont to DELETE /elements
+    // fn delete_all(conn: &mut SqliteConnection) -> Result<usize, ErrorCrud> {
+    //     Ok(diesel::delete(Self::table()).execute(conn)?)
+    // }
 }
